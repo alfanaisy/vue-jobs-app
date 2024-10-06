dffc54889bc696872897d9dc932efce2562d9afa
alfanaisy <alfanaisy7@gmail.com>
2024-10-07 00:10:17 +0700

    Add RouterLink

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, defineProps } from 'vue';
+import { RouterLink } from 'vue-router';
 
 defineProps({
   limit: Number,
@@ -33,10 +34,11 @@ console.log(jobs.value);
     </div>
   </section>
   <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-    <a
-      href="/jobs"
+    <RouterLink
+      to="/jobs"
       class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-      >View All Jobs</a
     >
+      View All Jobs
+    </RouterLink>
   </section>
 </template>
