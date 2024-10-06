e034ccf76868dd18e68776a37f197e9c3fa42e40
alfanaisy <alfanaisy7@gmail.com>
2024-10-07 02:07:30 +0700

    Add toast

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import './assets/main.css'
 import 'primeicons/primeicons.css'
+import './assets/main.css'
 import router from './router'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+
 const app = createApp(App)
 
 app.use(router)
+app.use(Toast)
 
 app.mount('#app')
